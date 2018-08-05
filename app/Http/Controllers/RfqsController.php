676cafd092901be8599db29670c3d60d6f8ead70
Abdulaziz Saud <azoz-saud@live.com>
2018-08-05 21:57:06 +0300

    renamed rfq controller

--- app/Http/Controllers/RfqsController.php
+++ app/Http/Controllers/RfqsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RfqController extends Controller
+class RfqsController extends Controller
 {
     /**
      * Display a listing of the resource.
