71bf04dc5b6aa061ed11d7ccd1d1b3ef9099bbe3
Abdulaziz Saud <azoz-saud@live.com>
2018-08-05 20:06:42 +0300

    added rfq and connected to the darabase

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>AMT</title>
-
-    </head>
-    <body>
-        <h1>About</h1>
-        <p>This is an application for the RFQ system in AMT.</p>
-    </body>
-</html>
+@section('content')
+<h1>About</h1>
+<p>This is an application for the RFQ system in AMT.</p>
+@endsection
