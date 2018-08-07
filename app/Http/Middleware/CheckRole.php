2f58dac5c38a0e602faac40d33c5d6df89793bfc
Abdulaziz Saud <azoz-saud@live.com>
2018-08-07 11:11:45 +0300

    gh

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $roles)
+    {
+        if($request->user() != null){
+            $rolesArr = explode(';', $roles);
+            if($request->user()->hasAnyRole($rolesArr))
+                return $next($request);
+        }
+
+        return response("You're not authorized");
+    }
+}
