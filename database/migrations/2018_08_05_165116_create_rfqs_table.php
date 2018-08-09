eed7771761ea4175d4a1bf2f11a1207a1d97b00f
Abdulaziz Saud <azoz-saud@live.com>
2018-08-09 03:02:45 +0300

    modified the rfqs table migration

--- database/migrations/2018_08_05_165116_create_rfqs_table.php
+++ database/migrations/2018_08_05_165116_create_rfqs_table.php
@@ -18,14 +18,27 @@ class CreateRfqsTable extends Migration
             $table->timestamps();
             $table->string('ref');
             $table->integer('created_by');
-            $table->integer('client_id');
             $table->string('status');
             $table->string('revision');
+            $table->integer('project_id');
+            $table->integer('client_id');
+            $table->integer('contact_id');
             $table->string('holder');
             $table->double('margin');
-
-
-
+            $table->integer('win_chance');
+            $table->integer('updated_by');
+            $table->string('receiving_method');
+            $table->string('delivery_place');
+            $table->string('relationship_level');
+            $table->string('bidding_time');
+            $table->string('expected_order_date');
+            $table->string('client_due_date');
+            $table->string('expected_completion_date');
+            $table->string('costing_starting_date');
+            $table->string('poposal_date');
+            $table->integer('presales_manager_id');
+            $table->integer('sales_mangaer_id');
+            $table->integer('sales_engineer_id');
         });
     }
 
