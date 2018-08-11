e9c2f2149d4938174bb2e4fff3983a979bfd271b
mohannad44 <mfaa321@gmail.com>
2018-08-11 18:48:37 +0300

    Intial layout design. i will dicuss it with you guys  later

---
+++ resources/views/layouts/cards/topright.blade.php
@@ -0,0 +1,8 @@
+<div class="col-md-6">
+    <div class="card bg-light mb-3" style="height: 20em; width: 30em;">
+        <div class="card-header">Statistics</div>
+        <div class="card-body">
+            <p>Numbers here!</p>
+        </div>
+    </div>
+</div>
