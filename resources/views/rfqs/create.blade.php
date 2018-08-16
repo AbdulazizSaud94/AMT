ea4ab6df8dc84eba3eaf8d6fc1c53c81003c9922
Abdulaziz Saud <azoz-saud@live.com>
2018-08-16 09:41:48 +0300

    changes

--- resources/views/rfqs/create.blade.php
+++ resources/views/rfqs/create.blade.php
@@ -70,7 +70,29 @@
 <hr>
 
 {{-- Select project and type --}}
-  <label class="ml-3"><b>Select project</b></label> <label class="ml-2">or</label> <a href="/laravel/AMT/public/projects/create" class="btn btn-primary btn-sm ml-3">Add new project</a>
+  <label class="ml-3"><b>Select project</b></label> <label class="ml-2">or</label>
+  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-sm ml-3" data-toggle="modal" data-target="#exampleModal">
+  Add new project
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add a project</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        dtcy
+      </div>
+
+    </div>
+  </div>
+</div>
 <div class="form-group col-md-2">
       <select class="form-control form-control-sm" name = "project_id">
         <option selected>Choose...</option>
