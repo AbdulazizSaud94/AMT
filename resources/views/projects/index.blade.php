a83445341261029634bd3c24d9d0fc97989792ad
Unknown <mfaa321@gmail.com>
2018-08-29 12:29:43 +0300

    fix some margins

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.master')
 @section('content')
-<div class="card m-4">
     <div class="card">
         <div class="card-header">
             <div class="row">
@@ -9,31 +8,30 @@
                 </div>
             </div>
         </div>
-            <div class="card-body">
-                @if(count($projects) > 0)
-                    {{-- Loop print the title, body, timestam for each post --}}
-                    @foreach($projects as $project)
-                        <div class="list-group">
-                            <a class="list-group-item list-group-item-action bg-light" href="projects/{{$project->id}}">
-                                {{$project->name}}
-                                <br>
-                                <span class="small">Project type: {{$project->type}}</span>
-                            </a>
-                        </div>
-                    @endforeach
+        <div class="card-body">
+            @if(count($projects) > 0)
+                {{-- Loop print the title, body, timestam for each post --}}
+                @foreach($projects as $project)
+                    <div class="list-group">
+                        <a class="list-group-item list-group-item-action bg-light" href="projects/{{$project->id}}">
+                            {{$project->name}}
+                            <br>
+                            <span class="small">Project type: {{$project->type}}</span>
+                        </a>
+                    </div>
+                @endforeach
 
-                @else {{-- if no post found --}}
-                <div class="alert alert-info">
-                    <span class="h3">No projects were found</span>
+            @else {{-- if no post found --}}
+            <div class="alert alert-info">
+                <span class="h3">No projects were found</span>
+            </div>
+            @endif
+            <div class="row mt-2">
+                <div class="col-md-6">
+                    <a href="./" class="btn btn-secondary float-md-left">Back</a>
                 </div>
-                @endif
-                <div class="row mt-2">
-                    <div class="col-md-6">
-                        <a href="./" class="btn btn-secondary float-md-left">Back</a>
-                    </div>
-                    <div class="col-md-6">
-                        <a href="projects/create" class="btn btn-primary float-md-right">Add project</a>
-                    </div>
+                <div class="col-md-6">
+                    <a href="projects/create" class="btn btn-primary float-md-right">Add project</a>
                 </div>
             </div>
         </div>
