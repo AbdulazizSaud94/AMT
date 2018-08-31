d1015b99a13715766980ee896caf9b57803c152f
AdnanAlghamdi <41329939+AdnanAlghamdi@users.noreply.github.com>
2018-08-31 19:10:11 +0300

    Correct the urls..

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,12 +9,12 @@
                 <li class="h5 text-white text-lg-center font-weight-bold">{{Auth::user()->title}}</li>
             </ul>
         </li>
-        <li class="border-top p-2 sidebar-link" onclick="window.location = '/'"><i class="material-icons align-bottom">dashboard</i> Dashboard</li>
-        <li class="border-top p-2 sidebar-link"  onclick="window.location = '/users'"><i class="material-icons align-bottom">contacts</i> Users</li>
-        <li class="border-top p-2 sidebar-link " onclick="window.location = '/rfqs'"><i class="material-icons align-bottom">assignment</i> RFQ</li>
-        <li class="border-top p-2 sidebar-link " onclick="window.location = '/projects'"> <i class="material-icons align-bottom">work</i> Project</li>
-        <li class="border-top p-2 sidebar-link " onclick="window.location = '/clients'"> <i class="material-icons align-bottom">group</i> Client</li>
-        <li class="border-top p-2 sidebar-link " onclick="window.location = '/systems'"> <i class="material-icons align-bottom">computer</i> System</li>
-        <li class="border-top p-2 sidebar-link border-bottom" onclick="window.location = '/divisions'"> <i class="material-icons align-bottom">category</i> Division</li>
+        <li class="border-top p-2 sidebar-link" onclick="window.location = '{{URL('/')}}'"><i class="material-icons align-bottom">dashboard</i> Dashboard</li>
+        <li class="border-top p-2 sidebar-link"  onclick="window.location = '{{URL('/users')}}'"><i class="material-icons align-bottom">contacts</i> Users</li>
+        <li class="border-top p-2 sidebar-link " onclick="window.location = '{{URL('/rfqs')}}'"><i class="material-icons align-bottom">assignment</i> RFQ</li>
+        <li class="border-top p-2 sidebar-link " onclick="window.location = '{{URL('/projects')}}'"> <i class="material-icons align-bottom">work</i> Project</li>
+        <li class="border-top p-2 sidebar-link " onclick="window.location = '{{URL('/clients')}}'"> <i class="material-icons align-bottom">group</i> Client</li>
+        <li class="border-top p-2 sidebar-link " onclick="window.location = '{{URL('/systems')}}'"> <i class="material-icons align-bottom">computer</i> System</li>
+        <li class="border-top p-2 sidebar-link border-bottom" onclick="window.location = '{{URL('/divisions')}}'"> <i class="material-icons align-bottom">category</i> Division</li>
     </ul>
 </aside>
