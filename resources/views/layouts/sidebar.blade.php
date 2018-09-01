0fca946ad9cfa9f99c57ef407379e3c202325941
Abdulaziz Saud <azoz-saud@live.com>
2018-09-02 01:40:47 +0300

    Only Admin and Super Admin can see the users link

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside>
 
     <ul class="list-unstyled">
-        <li class="m-2"><img src="/images/logow.png" alt="AMT Logo" class="img-fluid"/></li>
+        <li class="m-2"><img src="./images/logow.png" alt="AMT Logo" class="img-fluid"/></li>
 
         <li>
             <ul class="list-unstyled p-2">
@@ -10,7 +10,9 @@
             </ul>
         </li>
         <li class="border-top p-2 sidebar-link" onclick="window.location = '{{URL('/')}}'"><i class="material-icons align-bottom">dashboard</i> Dashboard</li>
+        @if(Auth::user()->hasRole('super admin') || Auth::user()->hasRole('admin'))
         <li class="border-top p-2 sidebar-link"  onclick="window.location = '{{URL('/users')}}'"><i class="material-icons align-bottom">contacts</i> Users</li>
+        @endif
         <li class="border-top p-2 sidebar-link" onclick="window.location = '{{URL('/rfqs')}}'"><i class="material-icons align-bottom">assignment</i> RFQ</li>
         <li class="border-top p-2 sidebar-link" onclick="window.location = '{{URL('/projects')}}'"> <i class="material-icons align-bottom">work</i> Project</li>
         <li class="border-top p-2 sidebar-link" onclick="window.location = '{{URL('/clients')}}'"> <i class="material-icons align-bottom">group</i> Client</li>
